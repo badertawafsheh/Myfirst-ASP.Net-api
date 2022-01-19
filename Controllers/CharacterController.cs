ddfb21ef09330dca1b8d332a68958e11e3b0cbdc
Bader Tawafsheh <68567544+badertawafsheh@users.noreply.github.com>
2022-01-19 10:37:51 +0200

    Routing

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using models.first_web_api;
+using System;
+using System.Collections.Generic;
+
+namespace first_web_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController: ControllerBase
+    {
+        private static List<Character> Test = new List<Character>()
+        {
+            new Character(),    
+            new Character{Name="Bilal"},
+            new Character{Name="Nader" , age=24}
+
+        };
+        
+        [HttpGet]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(Test);
+
+        }
+        [HttpGet]
+        [Route("GetFirstItem")]
+        public ActionResult <Character> GetSingle()
+        {
+            return Ok(Test[0]);
+        }
+        [HttpGet]
+        [Route("GetSecondItem")]
+        public ActionResult<Character> GetSingle2()
+        {
+            return Ok(Test[1]);
+        }
+
+
+    }
+
+}
---
+++ Models/RpgClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace models.first_web_api
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))] // thats use to see the content of array in browser :) 
+    public enum RpgClass
+    {
+            Bader , 
+            Nader , 
+            Rami 
+    }
+
+
+
+}
