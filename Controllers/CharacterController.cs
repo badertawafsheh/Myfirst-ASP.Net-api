4bda7b790ba2c3986179eaeb34ed77e561203326
Bader Tawafsheh <68567544+badertawafsheh@users.noreply.github.com>
2022-01-19 10:56:55 +0200

    Routing_Using_param

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,34 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using models.first_web_api;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace first_web_api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CharacterController: ControllerBase
+    public class CharacterController : ControllerBase
     {
         private static List<Character> Test = new List<Character>()
         {
-            new Character(),    
-            new Character{Name="Bilal"},
-            new Character{Name="Nader" , age=24}
+            new Character(),
+            new Character{Id=1,Name="Bilal"},
+            new Character{Id=2,Name="Nader" , age=24}
 
         };
-        
+
         [HttpGet]
         public ActionResult<List<Character>> Get()
         {
             return Ok(Test);
 
         }
-        [HttpGet]
-        [Route("GetFirstItem")]
-        public ActionResult <Character> GetSingle()
+        /*        
+         
+         *[HttpGet]
+         * [Route("GetFirstItem")]
+        
+        */
+        [HttpGet("GetFisrtItem")]
+        public ActionResult<Character> GetSingle()
         {
             return Ok(Test[0]);
         }
+
+
         [HttpGet]
         [Route("GetSecondItem")]
         public ActionResult<Character> GetSingle2()
@@ -36,6 +44,13 @@ namespace first_web_api.Controllers
             return Ok(Test[1]);
         }
 
+
+        // Routing using param 
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id)
+        {
+            return Ok(Test.FirstOrDefault(c => c.Id == id));
+        }
 
     }
 
