0063c4612b00701d4516915763064f9679366c9e
Bader Tawafsheh <68567544+badertawafsheh@users.noreply.github.com>
2022-01-20 11:13:14 +0200

    Implementing the DataContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using models.first_web_api;
+
+namespace first_web_api.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) :base(options)
+        {
+
+        }
+        public DbSet<Character> Characters { get; set; }
+    }
+}
