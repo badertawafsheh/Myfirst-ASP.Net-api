4deb107185f705186d2f1af2452b638ae26b5e3d
Bader Tawafsheh <68567544+badertawafsheh@users.noreply.github.com>
2022-01-20 15:51:12 +0200

    AuthonticatonRpos

---
+++ Data/AuthRepository.cs
@@ -0,0 +1,23 @@
+﻿using first_web_api.Models;
+using System.Threading.Tasks;
+
+namespace first_web_api.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        public Task<ServiceResponse<int>> Login(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> userExist(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Data/IAuthRepository.cs
@@ -0,0 +1,13 @@
+﻿using first_web_api.Models;
+using System.Threading.Tasks;
+
+namespace first_web_api.Data
+{
+    public interface IAuthRepository
+    {
+        Task<ServiceResponse<int>> Register(User user, string password);
+        Task<ServiceResponse<int>> Login(string username, string password);
+        Task<bool> userExist(string username);
+
+    }
+}
