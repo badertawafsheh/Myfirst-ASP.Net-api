b836cd0a8ca55db56bad773292ea0661423efa44
Bader Tawafsheh <68567544+badertawafsheh@users.noreply.github.com>
2022-01-20 11:18:51 +0200

    ConnectionString & Adding the DbContext

--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,10 @@
+using first_web_api.Data;
 using first_web_api.Services.CharacterService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,7 +29,7 @@ namespace first_web_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<DataContext>(options=>options.UseSqlServer((Configuration.GetConnectionString("DefaultConnection"))));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
