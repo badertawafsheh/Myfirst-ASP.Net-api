55dfc20573e349f5b70aa0c09421743cbedf57d1
Bader Tawafsheh <68567544+badertawafsheh@users.noreply.github.com>
2022-01-21 18:52:06 +0200

    FAKE Commit

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -5,19 +5,32 @@ namespace first_web_api.Data
 {
     public class AuthRepository : IAuthRepository
     {
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+
+        }
         public Task<ServiceResponse<int>> Login(string username, string password)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<ServiceResponse<int>> Register(User user, string password)
+        public async Task<ServiceResponse<int>> Register(User user, string password)
         {
-            throw new System.NotImplementedException();
-        }
+            _context.Users.Add(user);   
+            await _context.SaveChangesAsync();
+            ServiceResponse<int> response = new ServiceResponse<int>();
+            response.Data = user.Id;
+            return response;    
+        } 
 
         public Task<bool> userExist(string username)
         {
             throw new System.NotImplementedException();
         }
+
+        
+
     }
 }
